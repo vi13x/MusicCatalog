52f0b20399d27ba7e53295e39c0575d57fcf3b74
VŁ∆Ð <lakotko.vlad@mail.ru>
2026-02-26 00:14:53 +0300

    Delete src directory

--- src/main/java/com/example/musiccatalog/dto/ArtistDTO.java
+++
@@ -1,4 +0,0 @@
-package com.example.musiccatalog.dto;
-
-public record ArtistDTO(Long id, String name) {
-}
--- src/main/java/com/example/musiccatalog/dto/PlaylistDTO.java
+++
@@ -1,6 +0,0 @@
-package com.example.musiccatalog.dto;
-
-import java.util.Set;
-
-public record PlaylistDTO(Long id, String name, Set<TrackDTO> tracks) {
-}
