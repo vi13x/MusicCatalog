49f76abed6b9f5a1f0e6512c1d22336852554cf6
Lakotko Vladislav <vladislavlakotko@gmail.com>
2026-03-11 19:41:53 +0300

    test

--- src/main/java/com/example/musiccatalog/controller/MainController.java
+++ src/main/java/com/example/musiccatalog/controller/MainController.java
@@ -3,8 +3,6 @@ package com.example.musiccatalog.controller;
 import com.example.musiccatalog.service.DemoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,14 +19,14 @@ public class MainController {
         this.demoService = demoService;
     }
 
-    @PostMapping("/nplus1")
-    public ResponseEntity<String> nPlusOne(@RequestBody List<Long> albumIds) {
+    @GetMapping("/nplus1")
+    public ResponseEntity<String> nPlusOne(@RequestParam List<Long> albumIds) {
         int tracks = demoService.nPlusOneDemo(albumIds);
         return ResponseEntity.ok("Total tracks loaded (N+1 triggered): " + tracks);
     }
 
-    @PostMapping("/nplus1-fixed")
-    public ResponseEntity<String> nPlusOneFixed(@RequestBody List<Long> albumIds) {
+    @GetMapping("/nplus1-fixed")
+    public ResponseEntity<String> nPlusOneFixed(@RequestParam List<Long> albumIds) {
         int tracks = demoService.nPlusOneFixed(albumIds);
         return ResponseEntity.ok("Total tracks loaded (EntityGraph, no N+1): " + tracks);
     }
